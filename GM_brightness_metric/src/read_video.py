95aad0b9cd857d7c1c97de82245c515e620dbb3e
Pepper Potts <pepper@potts.com>
2020-03-06 18:14:09 +0100

    WIP update multithreading for video processing

--- GM_brightness_metric/src/read_video.py
+++ GM_brightness_metric/src/read_video.py
@@ -3,32 +3,48 @@ import cv2
 import numpy as np
 import concurrent.futures
 from gm_metric import GmMetric
+import itertools
+
+def image_process(image_info):
+    # is processing one image
+    status, image = image_info
+    print(GmMetric(image, 8).pq_1000())
+
 
 def read_video(inputpath):
     start_time = time.time()
     path, file = os.path.split(inputpath)
     cap = cv2.VideoCapture(inputpath)
+    
     current_frame = 1
     max_frames = 10
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     print(f'Start processing >> {file} << ')
     print(f'Total frames: {total_frames}')
-    gm_value_dict = {}
+
+    gm_values = []
     counter = 0
     # read video frame by frame
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        while (counter < max_frames):
-            current_frame = iter(list(range(1, total_frames+1, 1)))
-            gm_value_dict = executor.map(lambda frame: {next(current_frame): GmMetric(frame, 8).pq_1000()}, cap.read()[1])
-            counter += 1
+    while cap.isOpened():
+        # create slice of 10 images
+        # take 10 images and put them in a (queue) 
+        # batch = itertools.islice(cap.read(), 10)
+        # print(batch)
+        with concurrent.futures.ProcessPoolExecutor(max_workers=8) as executor:
+            gm_values = executor.map(image_process, cap.read())
 
-        print(f'Test mode activ - exit after {max_frames} frames')
-        print(f'Processing time: {round(time.time() - start_time, 2)} sec')
-        print(gm_value_dict)
-        cap.release()
-        cv2.destroyAllWindows()
-    return gm_value_dict
 
-if __name__ == "__main__":
+
+        # print(f'Test mode activ - exit after {max_frames} frames')
+        # print(f'Processing time: {round(time.time() - start_time, 2)} sec')
+        # print(gm_value_dict)
+    cap.release()
+    cv2.destroyAllWindows()
+    return gm_values
+
+def main():
     test_data = "/Users/pepper/Projekte/PythonProjects/image_analysis_repo/GM_brightness_metric/resources/video/Brosserness_4sec_h264_1920x1080_24fps_2Ch-stereo.mp4"
     read_video(test_data)
+
+if __name__ == "__main__":
+    main()
